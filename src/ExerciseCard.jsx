6d61a3a4ba1702b1fb7401bec4d4f56f37f892b8
ltorres24 <36682573+ltorres24@users.noreply.github.com>
2026-06-24 15:49:30 -0700

    Update ExerciseCard.jsx

--- src/ExerciseCard.jsx
+++ src/ExerciseCard.jsx
@@ -1,4 +1,15 @@
-export default function ExerciseCard({ name, goal, intensity, sets, onAddSet, onUpdateSet }) {
+export default function ExerciseCard({
+  name,
+  goal,
+  intensity,
+  sets,
+  onAddSet,
+  onUpdateSet,
+}) {
+  const youtubeSearchUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(
+    name + " exercise proper form"
+  )}`;
+
   return (
     <div className="exercise">
       <div className="exercise-head">
@@ -10,19 +21,31 @@ export default function ExerciseCard({ name, goal, intensity, sets, onAddSet, on
         </div>
       </div>
 
+      <a
+        className="video-link"
+        href={youtubeSearchUrl}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Watch Demo Video
+      </a>
+
       {sets.map((set, i) => (
         <div className="set-row" key={i}>
           <span>Set {i + 1}</span>
+
           <input
             placeholder="Weight"
             value={set.weight}
             onChange={(e) => onUpdateSet(i, "weight", e.target.value)}
           />
+
           <input
             placeholder="Reps"
             value={set.reps}
             onChange={(e) => onUpdateSet(i, "reps", e.target.value)}
           />
+
           <input
             placeholder="RPE"
             value={set.rpe}
